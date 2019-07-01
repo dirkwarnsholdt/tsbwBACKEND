747c77b8872827deaa6109d9b13e040e3b28874a
Jannis Rex <rex.jannis@web.de>
2019-07-01 16:12:29 +0200

    decode + acessing specific property
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

--- app/Http/Controllers/WeatherFetchController.php
+++ app/Http/Controllers/WeatherFetchController.php
@@ -12,7 +12,8 @@ class WeatherFetchController extends Controller
     public function index() {
         $url = 'http://api.openweathermap.org/data/2.5/weather?id=2897132&APPID=bda63977a6ec7a89b28153d79be9232f';
         $fetch = file_get_contents($url);
-        return $fetch;
+        $json = json_decode($fetch)
+        echo("return: " $json['wind'])
 
         // $json = json_decode($fetch);
         // if (!empty($json)) {
