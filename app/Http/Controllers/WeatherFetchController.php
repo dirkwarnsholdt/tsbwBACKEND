b9fa274469b938786eeac88fd7b0433bb043ba54
Jannis Rex <rex.jannis@web.de>
2019-07-01 11:21:21 +0200

    [changed] idk what but it works
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

--- app/Http/Controllers/WeatherFetchController.php
+++ app/Http/Controllers/WeatherFetchController.php
@@ -10,8 +10,8 @@ use App\Http\Requests;
 class WeatherFetchController extends Controller
 {
     public function giveHusum() {
-        $url = 'http://api.openweathermap.org/data/2.5/weather?id=2897132&APPID=bda63977a6ec7a89b28153d79be9232f'
-        $json = file_get_contents($url)
-        return $json
+        $url = 'http://api.openweathermap.org/data/2.5/weather?id=2897132&APPID=bda63977a6ec7a89b28153d79be9232f';
+        $json = file_get_contents($url);
+        return $json;
     }
 }
