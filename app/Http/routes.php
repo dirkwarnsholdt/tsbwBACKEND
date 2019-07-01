f4c416bfbdb154666e4717d2aa3b0ac641a574e4
Jannis Rex <rex.jannis@web.de>
2019-07-01 15:20:10 +0200

    asdo
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -50,7 +50,7 @@ Route::get('menuCALENDAR', 'MenuController@giveCALENDAR');
 //Weather - Api
 Route::get('weather', 'WeatherFetchController@index');
 Route::post('weather', 'WeatherFetchController@store');
-Route::patch('weather/0', 'WeatherFetchController@update');
+Route::patch('weather/1', 'WeatherFetchController@update');
 
 //Gallery
 Route::get('gallery', 'GalleryController@index');
