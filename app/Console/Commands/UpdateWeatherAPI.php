0494eac4003f9e5f2a17174794d0f2012ecda962
Jannis Rex <rex.jannis@web.de>
2019-07-01 13:01:09 +0200

    ..
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

--- app/Console/Commands/UpdateWeatherAPI.php
+++ app/Console/Commands/UpdateWeatherAPI.php
@@ -37,6 +37,17 @@ class UpdateWeatherAPI extends Command
      */
     public function handle()
     {
-        echo 'abc';
+        try {
+            $url = 'http://api.openweathermap.org/data/2.5/weather?id=2897132&APPID=bda63977a6ec7a89b28153d79be9232f';
+            $json = file_get_contents($url);
+            if (!empty($json)) {
+                echo 'not empty;';
+                return;
+            }
+        } catch (Exception $e) {
+            // only happens, when API
+            // is having problems, so nothing I should
+            // worry about :)
+        }
     }
 }
