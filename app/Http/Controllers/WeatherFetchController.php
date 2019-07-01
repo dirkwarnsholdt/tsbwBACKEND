cebd4867cad345cdfc060d277687c11a404f26e0
Jannis Rex <rex.jannis@web.de>
2019-07-01 10:39:53 +0200

    [added] file_get_contents => return
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

--- app/Http/Controllers/WeatherFetchController.php
+++ app/Http/Controllers/WeatherFetchController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Weather;
 
 use App\Http\Requests;
 
 class WeatherFetchController extends Controller
 {
-    public function giveHusum()
+    public function giveHusum() {
+        $url = 'http://api.openweathermap.org/data/2.5/weather?id=2897132&APPID=bda63977a6ec7a89b28153d79be9232f'
+        $json = file_get_contents($url)
+        return $json
+    }
 }
