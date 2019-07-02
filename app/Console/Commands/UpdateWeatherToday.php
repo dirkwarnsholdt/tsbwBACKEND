c9e0722eac7f6e3b74c589b7e5a742f503933cd6
Jannis Rex <rex.jannis@web.de>
2019-07-02 09:16:07 +0200

    need rel path aaakfsdfsdfixed
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

--- app/Console/Commands/UpdateWeatherToday.php
+++ app/Console/Commands/UpdateWeatherToday.php
@@ -55,8 +55,8 @@ class UpdateWeatherToday extends Command
             $url = 'http://api.openweathermap.org/data/2.5/weather?id=2897132&APPID=bda63977a6ec7a89b28153d79be9232f';
             $json = file_get_contents($url);
             file_put_contents('public/weather/weatherToday-TEMP.json', $json);
-            echo('REALPATH: ')
-            echo realpath(weatherToday-TEMP.json)
+            echo('REALPATH: ');
+            echo realpath(weatherToday-TEMP.json);
 
             // getting old file to overwrite
             try {
