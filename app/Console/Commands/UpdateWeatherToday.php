af8b271011de94c0ab3f8ea3b1e409afd17826c6
Jannis Rex <rex.jannis@web.de>
2019-07-02 08:18:20 +0200

    reee fixing .json save and read
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

--- app/Console/Commands/UpdateWeatherToday.php
+++ app/Console/Commands/UpdateWeatherToday.php
@@ -42,7 +42,7 @@ class UpdateWeatherToday extends Command
         try {
             $url = 'http://api.openweathermap.org/data/2.5/weather?id=2897132&APPID=bda63977a6ec7a89b28153d79be9232f';
             $json = file_get_contents($url);
-            file_put_contents('./weather/weatherToday.json', $json);
+            file_put_contents('/weather/weatherToday.json', $json);
 
         } catch (Exception $e) {
             echo('UPDATEweatherApi -- $json === empty');
