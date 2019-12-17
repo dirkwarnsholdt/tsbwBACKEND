02062fead37117281e77cc0c79e53a2da98cc4c6
Jannis Rex <rex.jannis@web.de>
2019-12-17 10:33:26 +0100

    [reverted] changes, since I need call to $gate and Illuminate\Auth\Access\Gate doesnt seem to work proper
    Signed-off-by: Jannis Rex <rex.jannis@web.de>

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,10 +1,7 @@
 <?php
-
 namespace App\Providers;
-
 use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-
 class AuthServiceProvider extends ServiceProvider
 {
     /**
@@ -15,33 +12,27 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
     ];
-
     /**
      * Register any application authentication / authorization services.
      *
      * @param  \Illuminate\Contracts\Auth\Access\Gate  $gate
      * @return void
      */
-    public function boot()
+    public function boot(GateContract $gate)
     {
-        $this->registerPolicies();
-
+        $this->registerPolicies($gate);
         $gate->define('see-news', function ($user) {
             return $user->news;
         });
-
         $gate->define('see-offer_detail', function ($user) {
             return $user->offer_detail;
         });
-
         $gate->define('see-person', function ($user) {
             return $user->person;
         });
-
         $gate->define('see-time', function ($user) {
             return $user->time;
         });
-
         $gate->define('see-menu', function ($user) {
             return $user->menu;
         });
